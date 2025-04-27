d94de8fb3d18f17dc4cdeec7309aba1f3b1df408
jose5556 <josefilmeneses@gamil.com>
2025-04-27 09:08:57 +0100

    enum

---
+++ CPP01/ex06/include/Harl.hpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/27 07:21:59 by cereais           #+#    #+#             */
+/*   Updated: 2025/04/27 09:06:05 by cereais          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+#include <string>
+
+#define DEBUG_MESSAGE "[ DEBUG ]\n love having extra bacon for my \
+7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"
+
+#define INFO_MESSAGE "[ INFO ]\nI cannot believe adding extra bacon costs more money. \
+You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!"
+
+#define WARNING_MESSAGE "[ WARNING ]\nI think I deserve to have some extra bacon for free. \
+I've been coming for years, whereas you started working here just last month."
+
+#define ERROR_MESSAGE "[ ERROR ]\nThis is unacceptable! I want to speak to the manager now."
+
+class Harl {
+	public:
+		Harl(void);
+		~Harl(void);
+		void complain( std::string level );
+	
+	private:
+		void debug( void );
+		void info( void );
+		void warning( void );
+		void error( void );
+};
+
+typedef void (Harl::*HarlFunc)(void);
+void	harldFilter(std::string	str, Harl harl);
+
+enum harlLevels {
+	DEBUG,
+	INFO,
+	WARNING,
+	ERROR	
+};
+
+#endif
