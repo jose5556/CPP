4fb2817f277f6926dd70deba00c3c1a0bd02d641
jose5556 <josefilmeneses@gmail.com>
2025-05-02 00:51:56 +0100

    main

--- CPP02/ex00/src/main.cpp
+++ CPP02/ex00/src/main.cpp
@@ -6,14 +6,23 @@
 /*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 02:55:34 by cereais           #+#    #+#             */
-/*   Updated: 2025/05/01 03:17:02 by cereais          ###   ########.fr       */
+/*   Updated: 2025/05/01 23:07:12 by cereais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Fixed.hpp"
 
-int	main(void) {
+int	main( void ) {
 
-	
-	
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return 0;
 }
