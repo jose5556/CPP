c31e2dc8b05ddb81a43199c648439ea631f768f6
jose5556 <josefilmeneses@gmail.com>
2025-05-04 11:02:51 +0100

    main

--- CPP02/ex02/src/main.cpp
+++ CPP02/ex02/src/main.cpp
@@ -3,29 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 02:55:34 by cereais           #+#    #+#             */
-/*   Updated: 2025/05/02 19:10:37 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/05/04 10:18:21 by cereais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Fixed.hpp"
 
 int main( void ) {
-
-Fixed a;
-Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-
-std::cout << a << std::endl;
-std::cout << ++a << std::endl;
-std::cout << a << std::endl;
-std::cout << a++ << std::endl;
-std::cout << a << std::endl;
-
-std::cout << b << std::endl;
-
-std::cout << Fixed::max( a, b ) << std::endl;
-
-return 0;
+	
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	
+	std::cout << b << std::endl;
+	
+	std::cout << Fixed::max( a, b ) << std::endl;
+	
+	return 0;
 }
