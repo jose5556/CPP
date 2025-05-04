8c4031d9c4d406ef9aeeb7532268adcd5c5cddd6
jose5556 <josefilmeneses@gmail.com>
2025-05-04 11:03:41 +0100

    all prototypes funcs of the Fixed class

--- CPP02/ex02/include/Fixed.hpp
+++ CPP02/ex02/include/Fixed.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 02:52:26 by cereais           #+#    #+#             */
-/*   Updated: 2025/05/02 18:59:31 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/05/04 11:02:25 by cereais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,40 @@ class Fixed {
 	
 public:
 
+	//Orthodox Canonical Form
 	Fixed( void );
 	Fixed( const int value );
 	Fixed( const float value );
-
 	Fixed(Fixed const & src);
-
 	~Fixed ( void );
+	Fixed &	operator=(Fixed const& raw);
 
-	Fixed &	operator=(Fixed const & raw);
+	//comparison operators:
+	bool	operator==(Fixed const& raw);
+	bool	operator!=(Fixed const& raw);
+	bool	operator<(Fixed const& raw);
+	bool	operator<=(Fixed const& raw);
+	bool	operator>(Fixed const& raw);
+	bool	operator>=(Fixed const& raw);
 
+	//arithmetic operators:
+	Fixed	operator+(Fixed const& raw) const;
+	Fixed &	operator-(Fixed const& raw) const;
+	Fixed &	operator*(Fixed const& raw) const;
+	Fixed &	operator/(Fixed const& raw) const;
+	
+	//increment/decrement operators:
+	
+	
+	//member functions:
 	int getRawBits( void ) const;
 	void setRawBits( int const raw );
 	float toFloat( void ) const;
 	int toInt( void ) const;
+	static const void*	min(void* n1, int* n2);
+	static const void*	min(int* const n1, int* const n2);
+	static const void*	max(int* n1, int* n2);
+	static const void*	max(int* const n1, int* const n2);
 	
 private:
 
