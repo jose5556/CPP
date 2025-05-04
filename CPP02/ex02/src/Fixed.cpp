b5ec2fcf32dc3ac402f7cb6fb313e72f9b79ee92
jose5556 <josefilmeneses@gmail.com>
2025-05-04 11:03:18 +0100

    arithmetic and comparisons overload operators

--- CPP02/ex02/src/Fixed.cpp
+++ CPP02/ex02/src/Fixed.cpp
@@ -3,49 +3,117 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 22:45:31 by cereais           #+#    #+#             */
-/*   Updated: 2025/05/02 17:50:46 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/05/04 11:01:50 by cereais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Fixed.hpp"
 
+//constructor/destructor
 Fixed::Fixed( void ) :_fixedPoint(0){
-
-	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed( const int value ) : _fixedPoint(value << _rawBits) {
-
-	std::cout << "Int constructor called" << std::endl;
 }
 
 Fixed::Fixed( const float value ) : _fixedPoint(roundf(value * (1 << _rawBits))){
-
-	std::cout << "Float constructor called" << std::endl;
 }
 
 Fixed::Fixed(Fixed const & src) {
-
-	std::cout << "Copy constructor called" << std::endl;
 	*this = src;
 }
 
 Fixed::~Fixed( void ) {
-
-	std::cout << "Destructor called" << std::endl;
 }
 
-Fixed& Fixed::operator=(Fixed const & raw) {
+Fixed& Fixed::operator=(Fixed const& raw) {
 	
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &raw)
-		this->_fixedPoint = raw._fixedPoint;
+	this->_fixedPoint = raw._fixedPoint;
 	return (*this);
 }
 
+//comparison operators
+bool	Fixed::operator==(Fixed const& raw) {
+
+	if (_fixedPoint == raw._fixedPoint)
+		return (true);
+	return (false);
+}
+
+bool	Fixed::operator!=(Fixed const& raw) {
+	
+	if (_fixedPoint != raw._fixedPoint)
+		return (true);
+	return (false);
+}
+
+bool	Fixed::operator<(Fixed const& raw) {
+	
+	if (_fixedPoint < raw._fixedPoint)
+		return (true);
+	return (false);
+}
+
+bool	Fixed::operator<=(Fixed const& raw) {
+	
+	if (_fixedPoint <= raw._fixedPoint)
+		return (true);
+	return (false);
+}
+
+bool	Fixed::operator>(Fixed const& raw) {
+	
+	if (_fixedPoint > raw._fixedPoint)
+		return (true);
+	return (false);
+}
+
+bool	Fixed::operator>=(Fixed const& raw) {
+	
+	if (_fixedPoint >= raw._fixedPoint)
+		return (true);
+	return (false);
+}
+
+//arithmetic operators
+Fixed	Fixed::operator+(Fixed const& raw) const {
+	
+	Fixed	temp;
+
+	temp = _fixedPoint + raw._fixedPoint;
+	return (temp);
+}
+
+Fixed &	Fixed::operator-(Fixed const& raw) const {
+	
+	Fixed	temp;
+
+	temp = _fixedPoint - raw._fixedPoint;
+	return (temp);
+}
+
+Fixed &	Fixed::operator/(Fixed const& raw) const {
+	
+	Fixed	temp;
+
+	temp = _fixedPoint / raw._fixedPoint;
+	return (temp);
+}
+
+Fixed &	Fixed::operator*(Fixed const& raw) const {
+	
+	Fixed	temp;
+
+	temp = _fixedPoint * raw._fixedPoint;
+	return (temp);
+}
+
+//member functions
 float	Fixed::toFloat( void ) const {
 	
 	return ((float)_fixedPoint / (1 << _rawBits));
@@ -56,6 +124,10 @@ int	Fixed::toInt( void ) const {
 	return (_fixedPoint >> _rawBits);
 }
 
+
+
+
+//getter/setter
 int	Fixed::getRawBits( void ) const {
 	
 	std::cout << "getRawBits member function called" << std::endl;
@@ -68,8 +140,9 @@ void	Fixed::setRawBits( int const raw ) {
 	this->_fixedPoint = raw;
 }
 
+//extra operator
 std::ostream& operator<<(std::ostream& out, const Fixed& valueFixedPoint) {
-
+	
 	out << valueFixedPoint.toFloat();
 	return (out);
 }
