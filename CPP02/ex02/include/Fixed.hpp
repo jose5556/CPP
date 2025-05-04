0b8c519c5df0e56e77b89b746870530551843357
jose5556 <josefilmeneses@gmail.com>
2025-05-04 12:00:39 +0100

    fixed min/max funcs prototypes

--- CPP02/ex02/include/Fixed.hpp
+++ CPP02/ex02/include/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 02:52:26 by cereais           #+#    #+#             */
-/*   Updated: 2025/05/04 11:02:25 by cereais          ###   ########.fr       */
+/*   Updated: 2025/05/04 11:56:38 by cereais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,32 +29,35 @@ public:
 	Fixed &	operator=(Fixed const& raw);
 
 	//comparison operators:
-	bool	operator==(Fixed const& raw);
-	bool	operator!=(Fixed const& raw);
-	bool	operator<(Fixed const& raw);
-	bool	operator<=(Fixed const& raw);
-	bool	operator>(Fixed const& raw);
-	bool	operator>=(Fixed const& raw);
+	bool	operator==(Fixed const& raw) const;
+	bool	operator!=(Fixed const& raw) const;
+	bool	operator<(Fixed const& raw) const;
+	bool	operator<=(Fixed const& raw) const;
+	bool	operator>(Fixed const& raw) const;
+	bool	operator>=(Fixed const& raw) const;
 
 	//arithmetic operators:
 	Fixed	operator+(Fixed const& raw) const;
-	Fixed &	operator-(Fixed const& raw) const;
-	Fixed &	operator*(Fixed const& raw) const;
-	Fixed &	operator/(Fixed const& raw) const;
-	
+	Fixed	operator-(Fixed const& raw) const;
+	Fixed	operator*(Fixed const& raw) const;
+	Fixed	operator/(Fixed const& raw) const;
+
 	//increment/decrement operators:
-	
-	
+	Fixed	operator++();
+	Fixed	operator++(int);
+	Fixed	operator--();
+	Fixed	operator--(int);
+
 	//member functions:
 	int getRawBits( void ) const;
 	void setRawBits( int const raw );
 	float toFloat( void ) const;
 	int toInt( void ) const;
-	static const void*	min(void* n1, int* n2);
-	static const void*	min(int* const n1, int* const n2);
-	static const void*	max(int* n1, int* n2);
-	static const void*	max(int* const n1, int* const n2);
-	
+	static const Fixed&	min(Fixed const& n1, Fixed const& n2);
+	static const Fixed&	min(Fixed& n1, Fixed& n2);
+	static const Fixed&	max(Fixed& n1, Fixed& n2);
+	static const Fixed&	max(Fixed const& n1, Fixed const& n2);
+
 private:
 
 	int					_fixedPoint;
