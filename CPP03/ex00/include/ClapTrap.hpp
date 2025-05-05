6c07f3a5813959ed31112cbcf1d62beb72597bc0
jose5556 <josefilmeneses@gmail.com>
2025-05-05 20:45:49 +0100

    ClapTrap class

---
+++ CPP03/ex00/include/ClapTrap.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 19:09:51 by cereais           #+#    #+#             */
+/*   Updated: 2025/05/05 19:33:35 by cereais          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class ClapTrap {
+
+	public:
+
+		ClapTrap();
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap& copy);
+		ClapTrap& operator=(const ClapTrap& scr);
+		~ClapTrap();
+
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+
+	private:
+
+		std::string _name;
+		int			_hitPoints;
+		int			_energyPoints;
+		int			_attackDamage;
+};
