700c7bdbc9fb93ae17296339399bda18af262f66
jose5556 <josefilmeneses@gmail.com>
2025-05-12 19:08:10 +0100

    diamond class

--- CPP03/ex03/src/DiamondTrap.cpp
+++ CPP03/ex03/src/DiamondTrap.cpp
@@ -6,7 +6,56 @@
 /*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 18:33:45 by cereais           #+#    #+#             */
-/*   Updated: 2025/05/09 18:33:46 by cereais          ###   ########.fr       */
+/*   Updated: 2025/05/12 18:12:03 by cereais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/DiamondTrap.hpp"
+
+DiamondTrap::DiamondTrap() : ClapTrap(), ScavTrap(), FragTrap() {
+	
+	this->_name = "Unknown";
+	this->_hitPoints = FragTrap::_hitPoints;
+	this->_energyPoints = ScavTrap::_energyPoints;
+	this->_attackDamage = FragTrap::_attackDamage;
+
+	std::cout << "DiamondTrap -> " << this->_name << " envolved to something, im a bit confused." << std::endl;
+}
+
+DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name"),
+	ScavTrap(name + "_clap_name"), FragTrap(name + "_clap_name") {
+
+	this->_name = name;
+	ClapTrap::_name = name + "_clap_name";
+	this->_hitPoints = FragTrap::_hitPoints;
+	this->_energyPoints = ScavTrap::_energyPoints;
+	this->_attackDamage = FragTrap::_attackDamage;
+
+	std::cout << "DiamondTrap -> " << this->_name << " envolved to something, im a bit confused." << std::endl;
+}
+
+DiamondTrap::DiamondTrap(const DiamondTrap& copy) : ClapTrap(copy), ScavTrap(copy), FragTrap(copy) {
+}
+
+DiamondTrap& DiamondTrap::operator=(const DiamondTrap& src) {
+	
+	if (this != &src) {
+		
+		this->_attackDamage = src._attackDamage;
+		this->_energyPoints = src._energyPoints;
+		this->_hitPoints = src._hitPoints;
+		this->_name = src._name;
+	}
+	return (*this);
+}
+
+DiamondTrap::~DiamondTrap() {
+	
+	std::cout << "DiamondTrap -> " << this->_name << " is... losing powers?" << std::endl;
+}
+
+void	DiamondTrap::whoAmI() {
+	
+	std::cout << "My ClapTrap name is " << ClapTrap::_name << std::endl;
+    std::cout << "My DiamondTrap name is " << this->_name << std::endl;
+}
