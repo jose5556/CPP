86912a0af7d4cc54e853c385d79ebf46cc3e92c5
jose5556 <josefilmeneses@gmail.com>
2025-05-12 19:08:21 +0100

    simple main to test the program

--- CPP03/ex03/src/main.cpp
+++ CPP03/ex03/src/main.cpp
@@ -6,23 +6,35 @@
 /*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 19:10:06 by cereais           #+#    #+#             */
-/*   Updated: 2025/05/09 18:28:13 by cereais          ###   ########.fr       */
+/*   Updated: 2025/05/12 18:07:47 by cereais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ClapTrap.hpp"
 #include "../include/ScavTrap.hpp"
 #include "../include/FragTrap.hpp"
+#include "../include/DiamondTrap.hpp"
 
 int	main(void) {
 	
-	FragTrap	roberto("roberto");
-	FragTrap	manuel("manuel");
+	DiamondTrap	roberto("roberto");
 
 	std::cout << std::endl;
 
-	//test if he can high five you UwU
-	roberto.highFivesGuys();
+	//test if he can show both names
+	roberto.whoAmI();
+
+	std::cout << std::endl;
+
+	//should use ScavTrap attack method
+	roberto.attack("pinguim");
+
+	std::cout << std::endl;
+
+	//should use ClapTrap take damage method
+	roberto.takeDamage(35);
+	roberto.takeDamage(35);
+	roberto.takeDamage(35);
 
 	std::cout << std::endl;
 
