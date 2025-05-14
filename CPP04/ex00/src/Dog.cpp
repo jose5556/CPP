ca67944ba28d9c45346e886928d8e86159e5cbd1
jose5556 <josefilmeneses@gmail.com>
2025-05-14 17:16:37 +0100

    creation of files

--- CPP04/ex00/src/Dog.cpp
+++ CPP04/ex00/src/Dog.cpp
@@ -1,29 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   dog.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/12 19:14:46 by cereais           #+#    #+#             */
-/*   Updated: 2025/05/14 17:16:14 by joseoliv         ###   ########.fr       */
+/*   Created: 2025/05/14 17:13:40 by joseoliv          #+#    #+#             */
+/*   Updated: 2025/05/14 17:13:41 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/Animal.hpp"
-#include "../include/Cat.hpp"
-#include "../include/Dog.hpp"
-
-int main()
-{
-const Dog* meta = new Animal();
-const Animal* j = new Dog();
-const Animal* i = new Cat();
-std::cout << j->getType() << " " << std::endl;
-std::cout << i->getType() << " " << std::endl;
-i->makeSound(); //will output the cat sound!
-j->makeSound();
-meta->makeSound();
-...
-return 0;
-}
