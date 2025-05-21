9e0f49c3c2b2686e9b1e8d7ef49cb68980548244
jose5556 <josefilmeneses@gmail.com>
2025-05-21 18:57:24 +0100

    class completelly defined

--- CPP04/ex03/include/AMateria.hpp
+++ CPP04/ex03/include/AMateria.hpp
@@ -3,12 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   AMateria.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
+/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 18:36:17 by cereais           #+#    #+#             */
-/*   Updated: 2025/05/20 18:47:04 by cereais          ###   ########.fr       */
+/*   Updated: 2025/05/21 18:49:32 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
 
 #include <string>
 #include <iostream>
@@ -17,11 +19,16 @@
 class AMateria {
 	
 	public:
+		AMateria();
 		AMateria(std::string const & type);
+		~AMateria();
+		AMateria(const AMateria& copy);
+		AMateria& operator=(const AMateria& src);
+		
 		std::string const & getType() const; //Returns the materia type
 		virtual AMateria* clone() const = 0;
 		virtual void use(ICharacter& target);
 
 	protected:
-		
+		std::string	_type;
 };
--- CPP04/ex03/src/AMateria.cpp
+++ CPP04/ex03/src/AMateria.cpp
@@ -3,10 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   AMateria.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
+/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 18:36:15 by cereais           #+#    #+#             */
-/*   Updated: 2025/05/20 18:36:16 by cereais          ###   ########.fr       */
+/*   Updated: 2025/05/21 18:54:01 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/AMateria.hpp"
+
+AMateria::AMateria() {
+}
+
+AMateria::AMateria(std::string const & type) : _type(type) {
+}
+
+AMateria::~AMateria() {
+}
+
+AMateria::AMateria(const AMateria& copy) {
+	
+	*this = copy;
+}
+
+AMateria& AMateria::operator=(const AMateria& src) {
+
+	if (this != &src) {
+		
+	}
+	return (*this);
+}
+
+std::string const & AMateria::getType() const {
+
+	return (this->_type);
+}
+
+void AMateria::use(ICharacter& target) {
+
+	std::cout << "Ice: \"* shoots an ice bolt at "
+	<< target.getName() << " *\"";
+
+	std::cout << "Cure: \"* heals "
+	<< target.getName() << "'s wounds *\"";
+}
