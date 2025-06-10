5761c0a22b1e112648493a4a70eeedd4f4afe041
jose5556 <josefilmeneses@gmail.com>
2025-06-10 06:16:39 +0100

    static cast for the argument

--- CPP06/ex00/src/ScalarConverter.cpp
+++ CPP06/ex00/src/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 02:53:11 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/06/10 04:57:12 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/06/10 06:15:29 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,22 +28,40 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& src) {
 	return (*this);
 }
 
-#include <stdlib.h>
-
 void	ScalarConverter::convert(const std::string &argument) {
 	
-	std::string type;
+	Type type;
 
 	if (argument.size() == 1 && !isdigit(argument[0]))
-		type = "char";
+		type = CHAR;
 	else if (isInt(argument))
-		type = "int";
+		type = INT;
 	else if (isFloat(argument))
-		type = "float";
+		type = FLOAT;
 	else if (isDouble(argument))
-		type = "double";
+		type = DOUBLE;
 	else
-		type = "ERROR!";
+		type = ERROR;
 
-	std::cout << type << std::endl;
+	switch (type) {
+		
+		case CHAR:
+			charTransform(argument);
+			break ;
+
+		case INT:
+			intTransform(argument);
+			break ;
+
+		case FLOAT:
+			floatTransform(argument);
+			break ;
+
+		case DOUBLE:
+			doubleTransform(argument);
+			break;
+
+		default:
+			std::cout << NOARGUMENTMATCH << std::endl;
+	}
 }
--- CPP06/ex00/src/converter.cpp
+++ CPP06/ex00/src/converter.cpp
@@ -6,26 +6,45 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 03:50:35 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/06/10 04:24:51 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/06/10 06:15:55 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ScalarConverter.hpp"
 #include "../include/converter.h"
 
-bool	isInt(std::string argument) {
+void	charTransform(const std::string &argument) {
 	
-	int		result = 0;
-	int		iIncrement = 0;
+	char	toChar;
+	int		toInt;
+	float	toFloat;
+	double	toDouble;
 
-	if (argument[0] == '-' || argument[0] == '+')
-		iIncrement++;
-	for (size_t i = iIncrement; i < argument.length(); i++) {
+	toChar = argument[0];
+	toInt = static_cast<char>(toChar);
+	toFloat = static_cast<char>(toChar);
+	toDouble = static_cast<char>(toChar);
 
-		if (argument[i] >= '0' && argument[i] <= '9') {
-			result = result * 10 + (argument[i] - '0');
-		} else
-			return (false);
-	}
-	return (true);
+	std::cout << "char: " << toChar << std::endl;
+	std::cout << "int: " << toInt << std::endl;
+	std::cout << "float: " << toFloat << ".0f" << std::endl;
+	std::cout << "double: " << toDouble << ".0" << std::endl;
+}
+
+void	intTransform(const std::string &argument) {
+	
+	char	toChar;
+	int		toInt;
+	float	toFloat;
+	double	toDouble;
+
+	toInt = argument[0];
+	toChar = static_cast<char>(toInt);
+	toFloat = static_cast<char>(toInt);
+	toDouble = static_cast<char>(toInt);
+
+	std::cout << "char: " << toChar << std::endl;
+	std::cout << "int: " << toInt << std::endl;
+	std::cout << "float: " << toFloat << ".0f" << std::endl;
+	std::cout << "double: " << toDouble << ".0" << std::endl;
 }
