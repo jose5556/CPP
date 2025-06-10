0214bf972e44696dca3792c67297af2de415bf1f
jose5556 <josefilmeneses@gmail.com>
2025-06-10 06:16:20 +0100

    file and prototypes

--- CPP06/ex00/include/converter.h
+++ CPP06/ex00/include/converter.h
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 03:17:32 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/06/10 05:18:23 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/06/10 06:15:14 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,22 @@
 #include <limits>
 
 #define ERRORARGUMENTS "ERROR! Wrong number of arguments"
+#define NOARGUMENTMATCH "ERROR! Invalid argument type"
+
+enum Type {
+
+	CHAR,
+	INT,
+	FLOAT,
+	DOUBLE,
+	ERROR
+};
 
 bool	isInt(const std::string &argument);
 bool	isFloat(const std::string &argument);
 bool	isDouble(const std::string &argument);
+
+void	charTransform(const std::string &argument);
+void	intTransform(const std::string &argument);
+void	floatTransform(const std::string &argument);
+void	doubleTransform(const std::string &argument);
