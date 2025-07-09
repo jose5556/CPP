1c7eda0eab5bbf44a901e22a2e9fef73c2f7d8ef
jose5556 <josefilmeneses@gmail.com>
2025-07-09 03:28:46 +0100

    array ex

---
+++ CPP07/ex02/include/Array.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/28 11:12:48 by cereais           #+#    #+#             */
+/*   Updated: 2025/07/09 03:27:39 by cereais          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <cctype>
+
+template <class T>
+class Array {
+
+public:
+    Array();
+	Array(unsigned int n);
+	Array(const Array& copy);
+	Array& operator=(const Array& src);
+	
+	int	size();
+
+class outOfBounds : public std::exception {
+	virtual const char* what() const throw();
+};
+	
+private:
+    T var;
+};
