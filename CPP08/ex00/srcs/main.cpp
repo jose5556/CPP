9440d27e67ec1d1e1d0a552f9b4237285c9e4a7b
jose5556 <josefilmeneses@gmail.com>
2025-07-22 11:52:42 +0100

    ex08 iniciated

---
+++ CPP08/ex00/srcs/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/28 10:58:13 by cereais           #+#    #+#             */
+/*   Updated: 2025/07/22 08:33:18 by cereais          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/whatever.hpp"
+
+int main(void) {
+	
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
