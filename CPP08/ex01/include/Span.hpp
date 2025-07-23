3b8827b401aeb43791f88fc9fc69b4142a5cba93
jose5556 <josefilmeneses@gmail.com>
2025-07-23 17:13:59 +0100

    member functions

--- CPP08/ex01/include/Span.hpp
+++ CPP08/ex01/include/Span.hpp
@@ -6,8 +6,33 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 15:39:48 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/07/23 15:39:49 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/07/23 17:13:30 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Span {
+
+public:
+	Span();
+	Span(unsigned int N);
+	~Span();
+	Span(const Span &copy);
+	Span& operator=(const Span &src);
+
+	void	addNumber(int number);
+	int		shortestSpan();
+	int		longestSpan();
+
+	class operationFailed : public std::exception {
+		
+	};
+
+private:
+	std::vector<int> v1;
+};
