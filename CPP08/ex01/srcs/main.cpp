738ec8c5d497d01187d8e5b74fd2774927886f55
jose5556 <josefilmeneses@gmail.com>
2025-07-23 17:13:50 +0100

    tests

--- CPP08/ex01/srcs/main.cpp
+++ CPP08/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 10:58:13 by cereais           #+#    #+#             */
-/*   Updated: 2025/07/23 15:39:54 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/07/23 15:55:39 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 int main(void) {
 	
+	Span sp = Span(5);
 
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+
+	return 0
 }
