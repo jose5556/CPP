54ecf5896a38c52f2f453a93ee34476fd6d8e968
jose5556 <josefilmeneses@gmail.com>
2025-07-23 11:14:18 +0100

    easyfind template func added, main also has my own tests

--- CPP08/ex00/include/easyfind.hpp
+++ CPP08/ex00/include/easyfind.hpp
@@ -6,15 +6,23 @@
 /*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 11:50:14 by cereais           #+#    #+#             */
-/*   Updated: 2025/07/22 11:51:51 by cereais          ###   ########.fr       */
+/*   Updated: 2025/07/23 11:11:16 by cereais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <algorithm>
+#include <iterator>
+#include <list>
+#include <vector>
 #include <iostream>
-#include <string>
 
-
-
-#include "easyfind.tpp"
+template <typename T>
+int	easyfind(const T &container, int b) {
+	
+	typename T::const_iterator it = std::find(container.begin(), container.end(), b);
+	if (it != container.end())
+		return (std::distance(container.begin(), it));
+	return (-1);
+}
--- CPP08/ex00/srcs/main.cpp
+++ CPP08/ex00/srcs/main.cpp
@@ -6,25 +6,25 @@
 /*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 10:58:13 by cereais           #+#    #+#             */
-/*   Updated: 2025/07/22 08:33:18 by cereais          ###   ########.fr       */
+/*   Updated: 2025/07/23 11:12:10 by cereais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/whatever.hpp"
+#include "../include/easyfind.hpp"
 
 int main(void) {
 	
-	int a = 2;
-	int b = 3;
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-	return 0;
+	int indexListResult;
+	int indexVectorResult;
+	
+	int arr[] = {1, 3, 5, 7, 9};
+	std::list<int> l(arr, arr + 5);
+	std::vector<int> v(arr, arr + 5);
+
+
+	indexListResult = easyfind(l, 9);
+	indexVectorResult = easyfind(v, 9);
+
+	std::cout << "list index is -> " << indexListResult << std::endl;
+	std::cout << "vector index is -> " << indexVectorResult << std::endl;
 }
