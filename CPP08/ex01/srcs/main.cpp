388bb83a5e995a9474ab1efb2f7284974d0cb81b
jose5556 <josefilmeneses@gmail.com>
2025-07-24 16:20:08 +0100

    tests

--- CPP08/ex01/srcs/main.cpp
+++ CPP08/ex01/srcs/main.cpp
@@ -3,18 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 10:58:13 by cereais           #+#    #+#             */
-/*   Updated: 2025/07/23 17:14:22 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/07/24 16:19:44 by cereais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Span.hpp"
+#include <cstdlib>
+#include <ctime>
 
-int main(void) {
+/* int main(void) {
 	
-	Span sp = Span(5);
+	Span sp = Span(10);
 
 	sp.addNumber(6);
 	sp.addNumber(3);
@@ -22,8 +24,40 @@ int main(void) {
 	sp.addNumber(9);
 	sp.addNumber(11);
 
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
+	std::vector<int> example;
+	example.push_back(90);
+	example.push_back(92);
+	example.push_back(94);
+	example.push_back(96);
+
+	sp.addRangeNumber(example.begin(), example.end());
+
+	std::cout << sp.shortestSpan() << std::endl; //2
+	std::cout << sp.longestSpan() << std::endl;  //14
 
 	return 0;
+} */
+
+int main() {
+
+    std::srand(std::time(NULL));
+    Span sp(10000);
+    //Span sp(9999);
+
+    for (int i = 0; i <= 10000; ++i) {
+        int value = std::rand();
+        try {
+            sp.addNumber(value);
+        } catch (std::exception &e) {
+            std::cerr << "Error adding number: " << e.what() << std::endl;
+        }
+    }
+
+    try {
+        std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+        std::cout << "Longest span: " << sp.longestSpan() << std::endl;
+    } catch (std::exception &e) {
+        std::cerr << "Span error: " << e.what() << std::endl;
+    }
+    return 0;
 }
