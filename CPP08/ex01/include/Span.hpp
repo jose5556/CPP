88115f173c91c9321e79efa8a21c47a167c0868d
jose5556 <josefilmeneses@gmail.com>
2025-07-24 16:20:25 +0100

    member functions

--- CPP08/ex01/include/Span.hpp
+++ CPP08/ex01/include/Span.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Span.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 15:39:48 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/07/23 17:13:30 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/07/24 16:07:08 by cereais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,38 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <limits>
 
 class Span {
 
 public:
-	Span();
 	Span(unsigned int N);
 	~Span();
 	Span(const Span &copy);
 	Span& operator=(const Span &src);
 
 	void	addNumber(int number);
+	
+	template <typename itInput> 
+	void	addRangeNumber(itInput begin, itInput end) {
+		
+		if (_size < _v1.size() + std::distance(begin, end))
+			throw (Span::operationFailed());
+
+		_v1.insert(_v1.end(), begin, end);
+	}
+
 	int		shortestSpan();
 	int		longestSpan();
 
 	class operationFailed : public std::exception {
-		
+	public:
+		const char* what() const throw();
 	};
 
 private:
-	std::vector<int> v1;
+	Span();
+	std::vector<int>	_v1;
+	unsigned int		_size;
 };
--- CPP08/ex01/srcs/Span.cpp
+++ CPP08/ex01/srcs/Span.cpp
@@ -3,10 +3,75 @@
 /*                                                        :::      ::::::::   */
 /*   Span.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 15:39:17 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/07/23 15:39:18 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/07/24 16:13:29 by cereais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/Span.hpp"
+
+Span::Span(unsigned int N) {
+	
+	_size = N;
+}
+
+Span::~Span() {}
+
+Span::Span(const Span &copy) {
+	
+	_size = copy._size;
+	_v1 = copy._v1;
+}
+
+Span& Span::operator=(const Span& src) {
+	
+	if (this != &src) {
+		_size = src._size;
+		_v1 = src._v1;
+	}
+	return (*this);
+}
+
+const char* Span::operationFailed::what() const throw() {
+	
+	return ("Size exceeded, can't add a new number!");
+}
+
+void	Span::addNumber(int number) {
+	
+	if (_size < _v1.size())
+		throw (Span::operationFailed());
+	_v1.push_back(number);
+}
+
+int		Span::shortestSpan() {
+	
+	int	result = std::numeric_limits<int>::max();;
+
+	if (_v1.empty() || _v1.size() < 2)
+		throw (Span::operationFailed());
+
+	std::sort(_v1.begin(), _v1.end());
+	std::vector<int>::iterator it = _v1.begin();
+	
+	while (it + 1 != _v1.end()) {
+
+		if (*(it + 1) - *it < result)
+			result = (*(it + 1)) - *it;
+		it++;
+	}
+	return (result);
+}
+
+int		Span::longestSpan() {
+	
+	if (_v1.empty() || _v1.size() < 2)
+		throw (Span::operationFailed());
+
+	int	min = *std::min_element(_v1.begin(), _v1.end());
+	int	max = *std::max_element(_v1.begin(), _v1.end());
+
+	return (max - min);
+}
