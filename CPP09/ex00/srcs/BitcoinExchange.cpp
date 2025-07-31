5a5af2e3c13a82a39887fbbc7a2ac952eee8f86e
jose5556 <josefilmeneses@gmail.com>
2025-07-31 17:33:18 +0100

    data base map is inicialized with success

--- CPP09/ex00/srcs/BitcoinExchange.cpp
+++ CPP09/ex00/srcs/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 15:34:42 by joseoliv          #+#    #+#             */
-/*   Updated: 2025/07/31 16:42:12 by joseoliv         ###   ########.fr       */
+/*   Updated: 2025/07/31 17:32:15 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,15 @@ BitcoinExchange::BitcoinExchange() {
 	if (!file.is_open())
 		argumentError();
 	
-	//test if header input is valid
+	//test if header dataBase is valid
 	getline(file, line);
-	if (!verifyHeader(line))
+	if (!(line == "date,exchange_rate"))
 		headerError(line);
 	
 	while (getline(file, line)) {
 			
-		mapParser(this->_dataBase, line);
+		dataBaseParser(line);
 	}
-	
 }
 
 BitcoinExchange::~BitcoinExchange() {
@@ -39,9 +38,32 @@ BitcoinExchange::~BitcoinExchange() {
 
 BitcoinExchange::BitcoinExchange(const BitcoinExchange& copy) {
 
+	this->_dataBase = copy._dataBase;
 }
 
 BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& src) {
 	
+	if (this != &src)
+		this->_dataBase = src._dataBase;
+	return (*this);
 }
 
+void	BitcoinExchange::dataBaseParser(std::string line) {
+	
+	std::string	date;
+	std::string	valueStr;
+	float		valueNum;
+	size_t		pos;
+
+	pos = line.find(",");
+
+	if (pos == std::string::npos) {
+		return ;
+	}
+	
+	date = line.substr(0, pos);
+	valueStr = line.substr(pos+1);
+	valueNum = atof(valueStr.c_str());
+	
+	this->_dataBase.insert(std::make_pair(date, valueNum));
+}
