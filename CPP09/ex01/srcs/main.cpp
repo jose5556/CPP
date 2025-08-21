f0a4453c5349ff8fc4f5930253e5cbcbc58f3c44
jose5556 <josefilmeneses@gmail.com>
2025-08-21 12:17:36 +0100

    main

--- CPP09/ex01/srcs/main.cpp
+++ CPP09/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 23:37:02 by cereais           #+#    #+#             */
-/*   Updated: 2025/08/19 23:50:23 by cereais          ###   ########.fr       */
+/*   Updated: 2025/08/21 11:53:44 by cereais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 
 int main(int argc, char *argv[]) {
 	
-	std::string	input;
-	
 	if (argc != 2) {
 		std::cout << "ERROR! Please make sure you introduce exactly one argument." << std::endl;
 		return (1);
 	}
-	input = argv[1];
 	
+	try {
+		RPN r(argv[1]);
+		r.printStack();
+	} catch (std::exception &e) {
+		std::cerr << e.what();
+	}
 }
+
