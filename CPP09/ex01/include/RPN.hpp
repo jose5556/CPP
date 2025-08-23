238621d5ad890f574bbf156c3e508e020781b7f5
jose5556 <josefilmeneses@gmail.com>
2025-08-23 23:25:38 +0100

    2 more exceptions

--- CPP09/ex01/include/RPN.hpp
+++ CPP09/ex01/include/RPN.hpp
@@ -6,40 +6,51 @@
 /*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 23:37:28 by cereais           #+#    #+#             */
-/*   Updated: 2025/08/22 21:46:42 by cereais          ###   ########.fr       */
+/*   Updated: 2025/08/23 23:24:38 by cereais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-
 #include <string>
 #include <iostream>
 #include <stack>
 #include <cctype>
+#include <sstream>
+#include <cstdlib>
 
-class RPN {
-	
+class RPN
+{
+
 public:
 	RPN(std::string input);
 	~RPN();
-	RPN(const RPN& copy);
-	RPN& operator=(const RPN& src);
+	RPN(const RPN &copy);
+	RPN &operator=(const RPN &src);
 
-	bool	verifyInput(std::string parsed);
-	void	printStack();
-	void	mathNums(std::string chr);
-	void	stackParser();
-	int		mathOperation(std::string num1, std::string token, std::string num2);
+	bool verifyInput(std::string parsed);
+	void printStack();
+	void stackParser(std::string element);
+	void mathOperation(int num1, int num2, std::string element);
 
-	class IlligalInput : public std::exception {
-		const char* what() const throw();
+	class IllegalInput : public std::exception
+	{
+		const char *what() const throw();
 	};
-	class Empty : public std::exception {
-		const char* what() const throw();
+	class Empty : public std::exception
+	{
+		const char *what() const throw();
+	};
+	class IllegalOperation : public std::exception
+	{
+		const char *what() const throw();
+	};
+	class IllegalDivision : public std::exception
+	{
+		const char *what() const throw();
 	};
 
 private:
 	RPN();
 	std::stack<std::string> _myStack;
-	int 					_result;
+	int _result;
 };
