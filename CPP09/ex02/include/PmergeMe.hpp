54a69566693f9213b47e60726a0fde01151ccc0f
jose5556 <josefilmeneses@gmail.com>
2025-08-25 15:21:56 +0100

    architecture iniciated and makefile ready

---
+++ CPP09/ex02/include/PmergeMe.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cereais <cereais@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/19 23:37:28 by cereais           #+#    #+#             */
+/*   Updated: 2025/08/25 15:19:12 by cereais          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <string>
+#include <iostream>
+#include <vector>
+#include <deque>
+
+template <typename T>
+class PmergeMe {
+
+public:
+	PmergeMe();
+	~PmergeMe();
+	PmergeMe(const PmergeMe& copy);
+	PmergeMe& operator=(const PmergeMe& src);
+
+private:
+	
+
+};
